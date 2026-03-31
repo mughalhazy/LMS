e872a6d2c2c0f6ef005b7849d8a0f7387c46c5aa
mughalhazy <mughalhazy@gmail.com>
2026-03-31 20:33:44 +0500

    Implement Pakistan payment adapters via adapter pattern

--- integrations/payment/__init__.py
+++ integrations/payment/__init__.py
@@ -1,10 +1,17 @@
 from .adapters import MockFailureAdapter, MockSuccessAdapter
+from .base_adapter import PaymentResult, TenantPaymentContext
+from .easypaisa_adapter import EasyPaisaAdapter
 from .flow import InMemoryInvoiceStore, PaymentFlowService
+from .jazzcash_adapter import JazzCashAdapter
 from .router import PaymentProviderRouter
 
 __all__ = [
     "MockFailureAdapter",
     "MockSuccessAdapter",
+    "PaymentResult",
+    "TenantPaymentContext",
+    "JazzCashAdapter",
+    "EasyPaisaAdapter",
     "InMemoryInvoiceStore",
     "PaymentFlowService",
     "PaymentProviderRouter",
